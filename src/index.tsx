32c96946bc76febd611d07044c7738a064cb9d10
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-05-30 00:44:01 +0530

    messed shit up pushing for overlsited to redo my bs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./styles/tailwind.generated.css";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="">
+      {/* Nav */}
+      <Navbar />
+
+      <h1 className="text-blue-300 font-black text-3xl">hi</h1>
+
+      {/* Body */}
+      {/* New Post Modal */}
+      {/* Button */}
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
